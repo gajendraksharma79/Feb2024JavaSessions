776d20d68317a1c5455a9ef81bcba255a342de55
Gajendra Sharma <gs.gajendraksharma@gmail.com>
2024-07-08 23:00:32 +0530

    Adding all Java Practice Code

---
+++ src/OOPInterface/TestHospital.java
@@ -0,0 +1,54 @@
+package OOPInterface;
+
+public class TestHospital {
+
+	public static void main(String[] args) {
+
+		FortisHospital fh = new FortisHospital();
+		fh.physioServices();
+		fh.physioServices(10);
+		fh.dentalServices();
+		fh.oncologyServices();
+		fh.gastroServices();
+		fh.neuroServices();
+		fh.gynecServices();
+
+		fh.emergencyServices();
+
+		fh.medicalTraining();
+		fh.pathologoyServices();
+		
+		System.out.println(USMedical.MIN_FEE);
+		System.out.println(FortisHospital.MIN_FEE);
+		
+		USMedical.billing();
+		FortisHospital.billing();
+		
+		fh.helpDesk();
+		fh.covidVaccination();
+		
+		fh.medicalNews();
+		fh.medicalRND();
+		fh.medicalFunding();
+		
+		
+
+		// top casting:
+		// child class object can be referred by parent interface ref variable:
+		
+		USMedical us = 	new FortisHospital();
+		us.oncologyServices();
+		us.physioServices();
+		us.dentalServices();
+		us.emergencyServices();
+		
+		
+		//down casting:
+		//FortisHospital f = new USMedical();
+		
+		
+		
+
+	}
+
+}
